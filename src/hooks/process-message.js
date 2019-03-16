cc80cc08d3232d1834af701e6c7284541db905f7
Tobias Steiner <tobinski@dodis.ch>
2019-03-16 18:56:35 +0100

    Add aiml support

--- src/hooks/process-message.js
+++ src/hooks/process-message.js
@@ -1,6 +1,10 @@
 // Use this hook to manipulate incoming or outgoing data.
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
 
+const AIMLInterpreter = require('aimlinterpreter');
+const aimlInterpreter = new AIMLInterpreter({name:'AI-Curator', age:'6'});
+aimlInterpreter.loadAIMLFilesIntoArray([__dirname + '/../aiml/std-65percent.aiml']);
+
 module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
   return async context => {
     const { data } = context;
@@ -22,8 +26,19 @@ module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
       // Add the current date
       createdAt: new Date().getTime()
     };
+    console.log(data.text);
+    return aimlInterpreter.findAnswerInLoadedAIMLFiles(data.text, function(answer, wildCardArray, input) {
+      // Override the original data (so that people can't submit additional stuff)
+      context.data = {
+        answer,
+        text: data.text,
+        // Add the current date
+        createdAt: new Date().getTime()
+      };
+      console.log(answer + ' | ' + wildCardArray + ' | ' + input);
 
-    // Best practise, hooks should always return the context
-    return context;
+      // Best practise, hooks should always return the context
+      return context;
+    });
   };
 };
